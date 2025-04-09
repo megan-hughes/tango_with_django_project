5211fe486379fc55e571473c36b336e133d61aad
megan-hughes <meganhughes96@hotmail.com>
2025-04-09 18:24:39 +0100

    Chapter 8 finished

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/megan/Desktop/Internet_Technology/tango_with_django_project/env/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
